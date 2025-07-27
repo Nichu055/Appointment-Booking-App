f97997683977ac3e1e28ca9339eedc80fcf91ee9
Nichu055 <ulasiebuka2@gmail.com>
2025-07-27 12:02:36 +0100

    update Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,html}"
+    "./src/**/*.{js,ts,jsx,tsx,css,html}",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
