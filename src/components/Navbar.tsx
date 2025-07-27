a59d47e762fe9b908ab76eae1fd674e3bd2926c7
Nichu055 <ulasiebuka2@gmail.com>
2025-07-27 17:57:23 +0100

    commented-out Home link from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <span className="text-xl font-bold text-blue-600">Appoint Booking</span>
         {/* Desktop links */}
         <div className="hidden md:flex">
-          <a href="#" className="text-gray-700 hover:text-blue-600 px-3">Home</a>
+          {/* <a href="#" className="text-gray-700 hover:text-blue-600 px-3">Home</a> */}
           <a href="#" className="text-gray-700 hover:text-blue-600 px-3">Features</a>
           <a href="#" className="text-gray-700 hover:text-blue-600 px-3">Login</a>
         </div>
@@ -27,7 +27,7 @@ const Navbar = () => {
         {/* Mobile dropdown */}
         {menuOpen && (
           <div className="md:hidden absolute right-4 top-full w-40 bg-white rounded shadow px-4 py-2 z-10">
-            <a href="#" className="block text-gray-700 hover:text-blue-600 py-2">Home</a>
+            {/* <a href="#" className="block text-gray-700 hover:text-blue-600 py-2">Home</a> */}
             <a href="#" className="block text-gray-700 hover:text-blue-600 py-2">Features</a>
             <a href="#" className="block text-gray-700 hover:text-blue-600 py-2">Login</a>
           </div>
