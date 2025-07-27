155117c8c7c794d7ad8b8ed432e52c34378aa5d3
Nichu055 <ulasiebuka2@gmail.com>
2025-07-27 12:48:42 +0100

    Create Footer and navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
-<footer className="bg-gray-100 mt-16">
-      <div className="container mx-auto px-4 py-6 text-center text-gray-500 text-sm">
+const Footer = () => (
+
+    <footer className="bg-gray-100 mt-16">
+        <div className="container mx-auto px-4 py-6 text-center text-gray-500 text-sm">
         &copy; {new Date().getFullYear()} Appointment Booking App. All rights reserved.
-      </div>
-</footer>
+        </div>
+    </footer>
+)
+
+export default Footer;
