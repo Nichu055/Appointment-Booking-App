dc087912a05cc27b52a666d5cfc4724f0865ba32
Nichu055 <ulasiebuka2@gmail.com>
2025-07-27 20:32:24 +0100

    route dashboard and profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import RootLayout from './components/Layout/RootLayout';
 import Home from './pages/Home';
 import Form from './pages/Form';
 import AdminLogin from './admin/Login';
+import AdminDashboard from './admin/Dashboard';
+import AdminProfile from './admin/Profile';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,16 @@ const router = createBrowserRouter([
   {
     path: '/admin',
     element: <AdminLogin />,
+  },
+  {
+    path: '/admin/dashboard',
+    element: <AdminDashboard />,
+    children: [
+      {
+        path: 'profile',
+        element: <AdminProfile />,
+      },
+    ],
   }
 ]);
 
