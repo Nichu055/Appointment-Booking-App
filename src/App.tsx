6920f25f52664fc4096b7429de381b03c8018fc6
Nichu055 <ulasiebuka2@gmail.com>
2025-07-27 12:49:11 +0100

    Set up routing with RootLayout and Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import RootLayout from './components/Layout/RootLayout';
 
-  return (
- <></>
-  )
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
